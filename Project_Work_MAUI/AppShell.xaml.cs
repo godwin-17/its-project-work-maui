2149e5b8fd57a1e6008977a2a3211b3ece15192e
Godwin <agblezeg@gmail.com>
2023-09-20 15:48:12 +0200

    Aggiungere i file di progetto.

---
+++ Project_Work_MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_Work_MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Project_Work_MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Project_Work_MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
