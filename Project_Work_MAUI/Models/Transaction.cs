561f3d6c63d3afdb94723a4d444185fb7a0c90a9
Godwin <agblezeg@gmail.com>
2023-09-28 09:53:27 +0200

    Create transaction model

---
+++ Project_Work_MAUI/Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿namespace Project_Work_MAUI.Models
+{
+    public class Categoryid
+    {
+        public string category { get; set; }
+        public string typology { get; set; }
+        public string id { get; set; }
+    }
+
+    public class RootTransaction
+    {
+        public List<Transaction> transactions { get; set; }
+    }
+    public class Transaction
+    {
+        public DateTime date { get; set; }
+        public int? balance { get; set; }
+        public int amount { get; set; }
+        public Categoryid categoryid { get; set; }
+        public string description { get; set; }
+    }
+}
