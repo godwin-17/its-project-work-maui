aa9703d6507f6ea23cf19ddcfa8953effdddb720
Godwin <agblezeg@gmail.com>
2023-09-28 17:29:52 +0200

    Change homepage and add details page navigation

--- Project_Work_MAUI/HomePage.xaml.cs
+++ Project_Work_MAUI/HomePage.xaml.cs
@@ -1,3 +1,4 @@
+using Project_Work_MAUI.Models;
 using Project_Work_MAUI.ViewModels;
 
 namespace Project_Work_MAUI;
@@ -10,5 +11,16 @@ public partial class HomePage : ContentPage
 		BindingContext = vm;
         (BindingContext as HomeViewModel)?.LoadData();
 
+    }
+
+    private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+        if (e.Parameter is Transaction transaction)
+        {
+            await Shell.Current.GoToAsync("DetailsPage", new Dictionary<string, object>
+            {
+                ["Transaction"] = transaction
+            });
+        }
     }
 }
