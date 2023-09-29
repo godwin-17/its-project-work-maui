f6cf834b115e12cf7fb2be1bf71fd480a63015c5
Godwin <agblezeg@gmail.com>
2023-09-29 10:04:04 +0200

    Add transactions logic

---
+++ Project_Work_MAUI/Models/TransactionRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_Work_MAUI.Models
+{
+    public class TransactionRequest
+    {
+        public string? iban { get; set; }
+        public int amount { get; set; }
+        public string categoryid { get; set; }
+        public string description { get; set; }
+    }
+}
